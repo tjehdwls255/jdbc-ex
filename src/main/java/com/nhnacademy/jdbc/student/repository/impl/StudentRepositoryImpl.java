4cdb4d8357c707172c609d0631a644ea3e0e00c0
1234 <a1234@1234ui-MacBookAir.local>
2026-03-18 15:40:46 +0900

    save

--- src/main/java/com/nhnacademy/jdbc/student/repository/impl/StudentRepositoryImpl.java
+++ src/main/java/com/nhnacademy/jdbc/student/repository/impl/StudentRepositoryImpl.java
@@ -4,6 +4,7 @@ import com.nhnacademy.jdbc.student.domain.Student;
 import com.nhnacademy.jdbc.student.repository.StudentRepository;
 import com.nhnacademy.jdbc.util.DbUtils;
 import lombok.extern.slf4j.Slf4j;
+
 import java.sql.*;
 import java.util.Optional;
 
@@ -11,36 +12,83 @@ import java.util.Optional;
 public class StudentRepositoryImpl implements StudentRepository {
 
     @Override
-    public int save(Connection connection, Student student){
+    public int save(Connection connection, Student student) {
         //todo#2 학생등록
         String sql = "insert into jdbc_students(id,name,gender,age) values(?,?,?,?)";
-        try(PreparedStatement statement = connection.prepareStatement(sql);){
-        statement.setString(1, student.getId());
-        }catch(SQLException e){
+        try (PreparedStatement statement = connection.prepareStatement(sql);) {
+            statement.setString(1, student.getId());
+            statement.setString(2, student.getName());
+            statement.setString(3, student.getGender().toString());
+            statement.setInt(3, student.getAge());
+
+            int result = statement.executeUpdate();
+            return result;
+
+        } catch (SQLException e) {
             throw new RuntimeException();
         }
-        return 0;
     }
 
     @Override
-    public Optional<Student> findById(Connection connection,String id){
+    public Optional<Student> findById(Connection connection, String id) {
         //todo#3 학생조회
+        String sql = "select * from jdbc_students where id = ?";
+        ResultSet rs = null;
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, id);
+            rs = preparedStatement.executeQuery();
 
+            if (rs.next()) {
+                Student student = new Student(rs.getString("id"),
+                        rs.getString("name"),
+                        Student.GENDER.valueOf(rs.getString("gender")),
+                        rs.getInt("age"),
+                        rs.getTimestamp("created_at").toLocalDateTime());
+                return Optional.of(student);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        } finally {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new RuntimeException();
+            }
+        }
         return Optional.empty();
     }
 
-    @Override
-    public int update(Connection connection,Student student){
-        //todo#4 학생수정
 
-        return 0;
+    @Override
+    public int update(Connection connection, Student student) {
+        //todo#4 학생수정
+        String sql = "update jdbc_students set name=?,gender=?,age=? where id=?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            int index = 0;
+            preparedStatement.setString(++index, student.getName());
+            preparedStatement.setString(++index, student.getGender().toString());
+            preparedStatement.setInt(++index, student.getAge());
+            preparedStatement.setString(++index, student.getId());
+
+            int result = preparedStatement.executeUpdate();
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
     }
 
     @Override
-    public int deleteById(Connection connection,String id){
+    public int deleteById(Connection connection, String id) {
         //todo#5 학생삭제
+        String sql = "delete from jdbc_students where id = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, id);
 
-        return 0;
+            int result = preparedStatement.executeUpdate();
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
     }
 
 }
